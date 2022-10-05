3d1b1a0f3b825644a3f4df852ce06f8107437ffb
Anna Zdyb <anna.zdyb@gmail.com>
2022-10-05 13:01:10 +0200

    fixed a  bug: when deleting Local Storage first added item logs wrong

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,7 +14,7 @@ export class ShoppingListComponent implements OnInit {
     JSON.parse(localStorage.getItem('ingredients')) || []
    ;
 
-  ingredientsLocal =  JSON.parse(localStorage.getItem('ingredients'));
+  ingredientsLocal = null;
 
   constructor() {}
 
